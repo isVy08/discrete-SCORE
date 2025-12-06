762516f6edfed83427ca32d665243ca67097c6f2
Vy Vo <52537680+isVy08@users.noreply.github.com>
2025-12-06 12:34:09 +1100

    Initial commit

--- model/trainer.py
+++ model/trainer.py
@@ -3,36 +3,74 @@ import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
-def find_leaf(model, xt, t):
-    logprob, _ = model(xt, t)
 
+def compute_score_sum(logits, method):
+    if method == 'entropy':
+        # v1: entropy
+        score = torch.sum(logits * logits.exp(), dim=1) 
+    elif method == 'variance':
+        # v2: variance 
+        score = torch.sum( (logits ** 2) * logits.exp(), dim=1) - torch.sum(logits * logits.exp(), dim=1) ** 2
+    elif method == 'renyi2': 
+        # v3: renyi entropy with alpha = 2   
+        score = torch.sum(logits ** 2, dim=1)                 
+    elif method == 'renyi0':
+        # v4: KL(u|p): renyi entropy with alpha = 0
+        score = - torch.sum(logits, dim=1)                    
+    else:
+        # v5: KL(p|u) + KL(u|p)
+        score = - torch.sum(logits, dim=1) + torch.sum(logits * logits.exp(), dim=1) 
+    score = torch.sum(score)
+    return score
+            
+
+def find_leaf(model, xt, t, loader, device, method='entropy'):
+    
     scores = []
     num_nodes = xt.shape[1]
+    score_per_node = [0] * num_nodes
     for i in range(num_nodes):
-        logits = logprob[:, i, :]
-        # score = torch.sum(logits * logits.exp(), dim=1) # entropy
-        score = torch.sum( (logits ** 2) * logits.exp(), dim=1) - torch.sum(logits * logits.exp(), dim=1) ** 2 # variance
+        score = 0
+        for ids in loader: 
+            xt_batch = xt[ids, :].to(device)
+            t_batch = t[ids,].to(device)
+            logprob, _ = model(xt_batch, t_batch)
+            logits = logprob[:, i, :]
+            _score_ = compute_score_sum(logits, method)
+            score += _score_
+
+    
         values = torch.unique(xt[:, i], return_counts=False)
-        score_per_node = torch.sum(score) / len(values)
-        scores.append(score_per_node.item())
+        score_per_node[i] = score.item() / len(values)
+
+    scores = score_per_node
     leaf = np.array(scores).argmin()
     return leaf
 
+
 class Trainer:
-    
-    def __init__(self, dataset, batch_size):
+
+    def __init__(self, dataset, batch_size, sample_size=None):
         
         
         self.dataset = dataset
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.method = 'entropy'
+        
+        if sample_size is not None:
+            dataset.X = dataset.X.sample(frac=1).reset_index(drop=True)
+            dataset.X = dataset.X.loc[:sample_size, list(range(dataset.num_nodes))]
 
-
-        dataset.X = dataset.X.loc[:, list(range(dataset.num_nodes))]
+        else: 
+            dataset.X = dataset.X.loc[:, list(range(dataset.num_nodes))]
+        
         data = dataset.X.values.copy()
 
+
+        
         self.data = torch.LongTensor(data)
 
-        self.indices = list(range(dataset.num_samples))
+        self.indices = list(range(dataset.X.shape[0]))
         self.batch_size = batch_size
         
 
@@ -51,18 +89,17 @@ class Trainer:
     def train(self, xt, model, optimizer, true_leaf, num_timesteps, num_epochs):
 
         t = torch.randint(0, num_timesteps, (xt.shape[0],), dtype=torch.long)
-        t = t.to(xt.device)
 
-        loader = DataLoader(self.indices, batch_size=self.batch_size, shuffle=True)
+        loader = DataLoader(self.indices, batch_size=self.batch_size, shuffle=False)
 
         pbar = tqdm(range(num_epochs))
         for epoch in pbar:
             total_loss = 0
             for ids in loader:
-                xt_batch = xt[ids, :]
-                t_batch = t[ids,]
-                
-                loss = model.loss(xt_batch, t_batch)
+                xt_batch = xt[ids, :].to(self.device)
+                t_batch = t[ids,].to(self.device)
+            
+                loss = model.loss(xt_batch, t_batch) 
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
@@ -70,7 +107,8 @@ class Trainer:
                 total_loss += loss.item()
 
             total_loss /= len(loader)
-    
-        pred_leaf = find_leaf(model, xt, t)
+
+
+        pred_leaf = find_leaf(model, xt, t, loader, self.device, self.method)
         correct = True if pred_leaf in true_leaf else False   
         return pred_leaf, correct
