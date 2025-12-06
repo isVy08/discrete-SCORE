0551545943dd0b2481d62bd0b8deb7623411fbd7
Vy Vo <52537680+isVy08@users.noreply.github.com>
2025-12-06 12:32:01 +1100

    Initial commit

--- real.py
+++ real.py
@@ -1,59 +1,44 @@
+import os, re
 import numpy as np
-import os
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from pgmpy.utils import get_example_model
 
 class RealDataset:
     def __init__(self, config_code):
         
-        if config_code == 'bridge':
-            from ucimlrepo import fetch_ucirepo
-            # https://archive.ics.uci.edu/dataset/18/pittsburgh+bridges
-            data = fetch_ucirepo(id=18) 
-  
-            # data (as pandas dataframes) 
-            df = data.data.features 
-            df = df.sample(frac=1)
+        name = config_code
+        file_path = f'dataset/storage/{config_code}.csv'
+        model = None
+        if not os.path.isfile(file_path):
+            model = get_example_model(model=name)
+            df = model.simulate(n_samples=int(20e3))
+            colnames = sorted([name for name in df.columns])
+            df = df[colnames]
+            df.to_csv(file_path, index=False)
 
-            colnames = ['ERECTED', 'MATERIAL', 'SPAN', 'LANES']
-
-            df = df.loc[:, colnames]
-            df = df.dropna(inplace=False)
-
-            for name in colnames: 
-                col = df.loc[:, name]
-                values = {name:i for i, name in enumerate(col.unique())}
-                df.loc[:, name] = col.map(values)
+        else:
+            df = pd.read_csv(file_path)
             
-            self.X = df
-            self.B_bin = np.zeros((len(colnames), len(colnames)))
-            edges = [(0,2), (1,2), (1,3)]
-            for i,j in edges: 
-                self.B_bin[i,j] = 1.0
+        colnames = [name for name in df.columns]
         
-        else: 
-            from pgmpy.utils import get_example_model
-            model = get_example_model(model=config_code)
-            file_path = f'dataset/storage/{config_code}.csv'
-            if not os.path.isfile(file_path):
-                df = model.simulate(n_samples=int(5e3))
-                colnames = sorted([name for name in df.columns])
-                df = df[colnames]
-                df.to_csv(file_path, index=False)
-    
-            else:
-                df = pd.read_csv(file_path)
-                
-                
-            colnames = [name for name in df.columns]
-            
+        dag_path = f'dataset/storage/dag-{name}.csv'
+        if not os.path.isfile(dag_path):
+            if model is None:
+                model = get_example_model(model=name)
             B_bin = np.zeros((df.shape[1], df.shape[1]))
             for vi, vj in model.edges():
                 i,j = colnames.index(vi), colnames.index(vj)
                 B_bin[i,j] = 1
+            df = pd.DataFrame(B_bin)
+            df.to_csv(dag_path, header=True, index=False)
             
-            self.B_bin = B_bin
-            from sklearn.preprocessing import LabelEncoder
-            self.X = df.apply(LabelEncoder().fit_transform)
+        else:
+            B_bin = pd.read_csv(dag_path).values
+        
+        self.B_bin = B_bin
+        
+        self.X = df.apply(LabelEncoder().fit_transform)
 
         self.X = self.X.rename(columns={name: i for i, name in enumerate(colnames)})
         self.X = self.X.astype('int')
